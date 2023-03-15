5774724d27a181b9fe24cc7f36f5c5937e1874b3
Rafael Peraza <rdavid_peraza@live.com>
2023-03-15 13:30:22 -0400

    changes to js

--- Listas o arrays sesion 6/ordenarArray.js
+++ Listas o arrays sesion 6/ordenarArray.js
@@ -3,6 +3,39 @@ const array = [2,5,2,3,6,5,8]
 console.log(array);
 
 array.sort((a,b) => {
-    return -1
+    if (a < b){
+        return +1
+    } else if(a > b) {
+        return -1
+    }else{
+        return 0;
+    }
 })
 console.log(array)
+
+//Ordenar unicamente arrays numericos
+const arrNumericos = [,9,5,6,2,1,4,6,2,125]
+arrNumericos.sort((a,b) => b - a)
+console.log(arrNumericos)
+
+// Interesante in arrays de objetos
+const listaObjetos = [
+    { nombre: 'Rafael', edad: 30 },
+    { nombre: 'Gabriela', edad: 26},
+    { nombre: 'Reyna', edad: 47},
+    { nombre: 'Elmer', edad: 49},
+    { nombre: 'Caro', edad: 28}
+]
+
+// listaObjetos.sort((a,b) =>{
+//     if(a.edad < b.edad){
+//         return -1
+//     } else if (a.edad > b.edad){
+//         return +1
+//     } else {
+//         return 0
+//     }
+// })
+
+listaObjetos.sort((a,b)=> a.edad - b.edad)
+console.log(listaObjetos)
