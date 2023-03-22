2c790712cb3ba0e2657177afef83c79d7d852441
Rafael Peraza <rdavid_peraza@live.com>
2023-03-22 14:15:42 -0400

    libraries homework done

---
+++ modulos/tarea de modulos/controller.js
@@ -0,0 +1,10 @@
+//Crea un archivo controller.js que exporte 2 funciones: suma(a, b) y multiplica(a, b)
+
+export const suma = (a,b) => a + b;
+
+
+
+// Multiplicacion
+export const mul = (a,b) => a * b;
+
+
---
+++ modulos/tarea de modulos/index.js
@@ -0,0 +1,9 @@
+import { suma, mul} from "./controller.js"; 
+import chalk from "chalk";
+
+const sum1 = suma(6,6)
+console.log(sum1)
+const mul1 = mul(12,12)
+
+
+console.log(chalk.green(mul1));
